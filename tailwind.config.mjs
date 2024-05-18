64a596145de7c81a4ccafa29775ed3ddb9ab9dc3
Cesar adrian Hernández Hernández <cesarahh3@gmail.com>
2024-05-18 14:19:25 -0600

    Change styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,21 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fill: {
-				"perso": '#CC522B',
-				"perso-hover": "#cf3d0c"
-			},
-			textColor: {
-				"perso": '#CC522B',
-				"perso-hover": "#cf3d0c"
-			},
-			backgroundColor: {
-				"perso": '#CC522B',
-				"perso-hover": "#cf3d0c"
-			}
-		},
-	},
+	theme: {},
 	plugins: [],
 }
